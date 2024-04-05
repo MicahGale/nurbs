3b351b59f0e3a819c6447fd403d6de6c606e52cd
Micah D. Gale <mg@gale.engineer>
2024-04-04 20:56:26 -0500

    Updated plot to make subplots.

--- stochastic_spline.py
+++ stochastic_spline.py
@@ -20,29 +20,27 @@ def normal(x, mu, sigma):
 min = 0.0
 max = 10.0
 bins = 10
-histo = regressor.HistogramRegressor(min, max, bins)
-spline = regressor.SplineRegressor(min, max, bins, 4)
-bezier = regressor.OrthoBezierRegressor(min, max, 6)
+regressors = [regressor.HistogramRegressor(min, max, bins)]
+for order in range(6):
+    regressors.append(regressor.OrthoBezierRegressor(min, max, (order + 1) * 2))
 
-for name, curve in [("spline", spline), ("bezier", bezier)]:
-    plt.clf()
-    curve.plot_bases()
-    plt.savefig(f"{name}_bases.png")
 
 mu = 5.0
 sigma = 1.0
-for i in range(1000):
-    x = np.random.normal(mu, sigma)
-    if x < min or x > max:
-        continue
-    for regressor in [histo, spline, bezier]:
-        regressor.score(x)
+xs = np.linspace(0, 10)
+fig, axes_rows = plt.subplots(5, 6, figsize=(30, 20))
+for samples, ax_row in zip([10, 100, 1000, 10_000, 100_000], axes_rows):
+    for i in range(samples):
+        x = np.random.normal(mu, sigma)
+        if x < min or x > max:
+            continue
+        for regressor in regressors:
+            regressor.score(x)
+    for i, (r, ax) in enumerate(zip(regressors[1:], ax_row)):
+        ax.plot(xs, normal(xs, mu, sigma), "k--", label="true curve")
+        regressors[0].plot(ax)
+        r.plot(ax)
+        ax.set_title(f"n={samples} order={(i+1)*2}")
+    [r.reset() for r in regressors]
 
-plt.clf()
-bezier.plot()
-spline.plot()
-histo.plot()
-x = np.linspace(0, 10)
-plt.plot(x, normal(x, mu, sigma), "k--", label="true curve")
-plt.legend()
 plt.savefig("regression.png")
